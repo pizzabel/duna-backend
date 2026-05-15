8ffaf8d9ecc59b5f854d9bcbf5455cec0aaa9e60
Tu Nombre <tu@email.com>
2026-05-15 04:50:27 -0500

    feat: endpoint GET /posts/upload-url para S3 presigned URL

--- src/modules/posts/posts.controller.ts
+++ src/modules/posts/posts.controller.ts
@@ -1,18 +1,40 @@
 import {
   Controller, Get, Post, Patch, Delete,
-  Body, Param, Req, HttpCode, HttpStatus,
+  Body, Param, Req, HttpCode, HttpStatus, Query,
 } from '@nestjs/common';
-import { PostsService }     from './posts.service';
-import { CreatePostDto }    from './dto/create-post.dto';
-import { UpdatePostDto }    from './dto/update-post.dto';
+import { PostsService } from './posts.service';
+import { CreatePostDto } from './dto/create-post.dto';
+import { UpdatePostDto } from './dto/update-post.dto';
+import { S3Client, PutObjectCommand } from '@aws-sdk/client-s3';
+import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
+import { randomUUID } from 'crypto';
 
 @Controller('posts')
 export class PostsController {
   constructor(private readonly postsService: PostsService) {}
 
+  @Get('upload-url')
+  async getUploadUrl(@Query('ext') ext = 'jpg') {
+    const s3 = new S3Client({ region: process.env.AWS_REGION });
+    const key = `posts/${randomUUID()}.${ext}`;
+    const command = new PutObjectCommand({
+      Bucket: process.env.S3_BUCKET,
+      Key: key,
+      ContentType: `image/${ext}`,
+    });
+    const url = await getSignedUrl(s3, command, { expiresIn: 300 });
+    const publicUrl = `https://${process.env.S3_BUCKET}.s3.${process.env.AWS_REGION}.amazonaws.com/${key}`;
+    return { url, publicUrl, key };
+  }
+
   @Post()
   create(@Body() dto: CreatePostDto, @Req() req: any) {
     return this.postsService.create(dto, req.user.userId);
+  }
+
+  @Get('user/me')
+  myPosts(@Req() req: any) {
+    return this.postsService.findByUser(req.user.userId);
   }
 
   @Get(':id')
@@ -29,10 +51,5 @@ export class PostsController {
   @HttpCode(HttpStatus.NO_CONTENT)
   remove(@Param('id') id: string, @Req() req: any) {
     return this.postsService.remove(id, req.user.userId);
-  }
-
-  @Get('user/me')
-  myPosts(@Req() req: any) {
-    return this.postsService.findByUser(req.user.userId);
   }
 }
